3fab1c53bbc64c1f85b0882c948a47c21bbd2264
Richard Styron <richard.h.styron@gmail.com>
2023-01-24 10:25:18 -0800

    updates

--- scripts/oceania_blocks.jl
+++ scripts/oceania_blocks.jl
@@ -12,7 +12,7 @@ using PyPlot
 
 # options
 geol_slip_rate_weight = 2.
-save_results = false
+save_results = true
 
 # load data
 
@@ -28,8 +28,11 @@ chn_slip_rate_file = "/home/itchy/research/gem/fault_data/china/block_data/geol_
 
 # jpn
 izu_tris_file = "/home/itchy/research/geodesy/global_block_comps/subduction/sub_tri_meshes/izu_slab2.geojson"
+
 # phl
 
+phl_block_file = "/home/itchy/research/geodesy/global_block_comps/phil_blocks/block_data/phl_blocks.geojson"
+phl_fault_file = "/home/itchy/research/geodesy/global_block_comps/phil_blocks/block_data/phl_faults.geojson"
 
 # glo
 glo_block_file = "/home/itchy/research/geodesy/global_block_comps/global_scale_plates/global_scale_plates.geojson"
@@ -51,16 +54,19 @@ ocn_bounds_file = "../block_data/oceania_bounds.geojson"
 
 # geodesy
 gsrm_vels_file = "/home/itchy/research/geodesy/gsrm/gps/gps_su.geojson"
+hsu_vels_file = "/home/itchy/research/geodesy/global_block_comps/phil_blocks/geod/hsu_luzon_campaign_vels.geojson"
 
 
 @info "joining blocks"
 glo_blocks = Oiler.IO.gis_vec_file_to_df(glo_block_file; fid_drop=["ant"])
 chn_blocks = Oiler.IO.gis_vec_file_to_df(chn_block_file)
-ocn_blocks = Oiler.IO.gis_vec_file_to_df(ocn_block_file; 
-                                         fid_drop=[ "ocn010"])
+ocn_blocks = Oiler.IO.gis_vec_file_to_df(ocn_block_file); 
+                                         #fid_drop=[ "ocn010"])
+phl_blocks = Oiler.IO.gis_vec_file_to_df(phl_block_file)
 
 block_df = vcat(ocn_blocks, 
                 glo_blocks, 
+                phl_blocks,
                 chn_blocks; 
                 cols=:union)
 
@@ -76,16 +82,22 @@ println("n blocks after ", size(block_df, 1))
 fault_df, faults, fault_vels = Oiler.IO.process_faults_from_gis_files(
     ocn_fault_file,
     chn_fault_file,
+    phl_fault_file,
     glo_fault_file;
     block_df=block_df,
     lsd_default=10.,
     e_default=10.,
+    check_blocks=true,
     #fid_drop=["ocnf054", "ocnf060"],
     subset_in_bounds=true)
 
 println("n faults: ", length(faults))
 println("n fault vels: ", length(fault_vels))
 
+@info "doing non-fault block boundaries"
+@time non_fault_bounds = Oiler.IO.get_non_fault_block_bounds(block_df, faults)
+bound_vels = vcat(map(Oiler.Boundaries.boundary_to_vels, non_fault_bounds)...)
+println("n non-fault-bound vels: ", length(bound_vels))
 
 @info "doing geologic slip rates"
 ocn_slip_rate_df = Oiler.IO.gis_vec_file_to_df(ocn_geol_slip_rate_file)
@@ -108,7 +120,16 @@ gsrm_vel_df = Oiler.IO.gis_vec_file_to_df(gsrm_vels_file)
     ve=:e_vel, vn=:n_vel, ee=:e_err, en=:n_err, name=:station
 )
 
-gnss_vels = gsrm_vels
+hsu_vel_df = Oiler.IO.gis_vec_file_to_df(hsu_vels_file)
+@time hsu_vels = Oiler.IO.make_vels_from_gnss_and_blocks(hsu_vel_df, block_df;
+    fix="ITRF08", epsg=102016,
+    ve=:ve, vn=:vn, ee=:ee, en=:en, name=:site
+)
+
+gnss_vels = vcat(
+             gsrm_vels,
+             hsu_vels,
+             )
 
 println("n gnss vels: ", length(gnss_vels))
 
@@ -125,12 +146,17 @@ sul_tris = Oiler.IO.tris_from_geojson(JSON.parsefile(sul_tris_file))
 sum_tris = Oiler.IO.tris_from_geojson(JSON.parsefile(sum_tris_file))
 png_tris = Oiler.IO.tris_from_geojson(JSON.parsefile(png_tris_file))
 van_tris = Oiler.IO.tris_from_geojson(JSON.parsefile(van_tris_file))
+cot_tris = Oiler.IO.tris_from_geojson(JSON.parsefile(cot_tris_file))
+phi_tris = Oiler.IO.tris_from_geojson(JSON.parsefile(phi_tris_file))
 
 tris = vcat(
             sol_tris,
             van_tris,
             #png_tris,
             sum_tris,
+            #cot_tris,
+            #phi_tris,
+            #sul_tris,
             )
 
 
